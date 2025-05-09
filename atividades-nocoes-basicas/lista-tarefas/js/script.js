ac6b66663641dde0237ab32adeb1aacce27ce3e9
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-05-09 17:06:01 -0300

    Lista de Tarefas

---
+++ atividades-nocoes-basicas/lista-tarefas/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const input = document.getElementById('tarefa');
+
+    const lista = document.getElementById('lista');
+
+    document.getElementById('adicionar').addEventListener('click', function () {
+
+        const item = document.createElement('li');
+        item.textContent = input.value;
+        lista.appendChild(item);
+        input.value = '';
+        
+    });
+})
