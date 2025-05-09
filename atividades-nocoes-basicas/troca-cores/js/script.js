93720d98d50c6f36aabb97cc8570fa15b768950a
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-05-09 01:54:25 -0300

    Trocador de Cores

---
+++ atividades-nocoes-basicas/troca-cores/js/script.js
@@ -0,0 +1,12 @@
+const quadrado = document.getElementById('quadrado');
+
+document.getElementById('vermelho').addEventListener('click', function () {
+    quadrado.style.backgroundColor = 'red';
+});
+document.getElementById('verde').addEventListener('click', function () {
+    quadrado.style.backgroundColor = 'green';
+});
+document.getElementById('azul').addEventListener('click', function () {
+    quadrado.style.backgroundColor = 'blue';
+});
+
