e25063f21bf790f47c5d821c82a615d0c04fed7e
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-05-09 01:41:09 -0300

    Contador de Caracteres

---
+++ atividades-nocoes-basicas/contador-de-caracteres/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const texto = document.getElementById('texto');
+    const contador = document.getElementById('contador');
+
+    texto.addEventListener('input', function () {
+        contador.textContent = texto.value.length + ' caracteres';
+    });
+});
