bf052afab87070bef1d1f023b9b07acfb77f34df
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-05-08 21:33:08 -0300

    Verificador de Número Positivo e Negativo

---
+++ atividades-nocoes-basicas/verificador-positivo-negativo/js/script.js
@@ -0,0 +1,21 @@
+document.getElementById('verificarBtn').addEventListener('click', function () {
+
+    const input = document.getElementById('numero');
+
+    const numero = parseFloat(input.value);
+
+    const resultado = document.getElementById('resultado');
+
+    if (numero > 0) {
+
+        resultado.textContent = 'O número é positivo.';
+        
+    } else if (numero < 0) {
+
+        resultado.textContent = 'O número é negativo.';
+
+    } else {
+        
+        resultado.textContent = 'O número é zero.';
+    }
+});
