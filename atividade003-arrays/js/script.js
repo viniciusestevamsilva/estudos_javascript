b043de57d5c718c1f91248746a85fe146fcf43ba
viniciusestevamsilva <viniciusestevamsilvajf@gmail.com>
2025-06-05 21:32:06 -0300

    feito

--- atividade003-arrays/js/script.js
+++ atividade003-arrays/js/script.js
@@ -1,8 +1,8 @@
 const produtos = [
-  { nome: "Sabonete Natural", preco: 15.0, categoria: "cosmetico" },
-  { nome: "Shampoo Organico", preco: 25.0, categoria: "cosmetico" },
-  { nome: "Granola Artesanal", preco: 18.5, categoria: "alimento" },
-  { nome: "Mel Puro", preco: 22.0, categoria: "alimento" },
+  { nome: "Sabonete Natural", preco: 15.00, categoria: "cosmetico" },
+  { nome: "Shampoo Organico", preco: 25.00, categoria: "cosmetico" },
+  { nome: "Granola Artesanal", preco: 18.05, categoria: "alimento" },
+  { nome: "Mel Puro", preco: 22.00, categoria: "alimento" },
 ];
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 0);
 
     
-    resultado.innerHTML = "R$ " + total;
+    resultado.innerHTML = `${total.toFixed(2)}`;
 
    
     listaProdutos.innerHTML = "";
@@ -41,13 +41,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
      
       const nome = document.createElement("p");
-      nome.classList.add("nome-produto");
       nome.textContent = produto.nome;
+      nome.style.fontWeight = 'bold'
 
       
       const preco = document.createElement("p");
-      preco.classList.add("preco-produto");
-      preco.textContent = "R$ " + produto.preco;
+    
+      preco.textContent = `R$ ${produto.preco.toFixed(2)}`;
 
       
       item.appendChild(nome);
